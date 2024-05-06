e223465f8972b2f0fb100a612407de62682a460c
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-05-06 10:35:30 +0300

    sad

--- Config/Dbnode.js
+++ Config/Dbnode.js
@@ -1,10 +1,10 @@
 const  mysql =require('mysql')
 const db= mysql.createPool({
-    host: 'bvkosdzx1dt9f9x4dws9-mysql.services.clever-cloud.com',
+    host: 'byizv8c6igtt4tglu5qy-mysql.services.clever-cloud.com',
     port:'3306',
-    user: 'ubupobfmgyd4muvw',
-    password: 'lvpOj9F6gz6klDFQY3iR',
-    database: 'bvkosdzx1dt9f9x4dws9'
+    user: 'u8fgkmjr89kmcgno',
+    password: 'U5ZmhGHJBmKHTgSCM4lo',
+    database: 'byizv8c6igtt4tglu5qy'
 });
 
  db.getConnection(()=>{
