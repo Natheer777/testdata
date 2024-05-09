603dbc0d2703c05f17cc6b5ddf52af4cb8968e21
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-05-09 06:35:02 +0300

    ss

--- index.js
+++ index.js
@@ -37,50 +37,20 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
+const route = require('../server/Routers/Route'); // Import your route file
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Mount your routes under /api using the 'route' variable
-const route = require('../server/Routers/Route');
-app.use(route);
+app.use(route); // Mount your routes under /api
 
-// Configure multer for file uploads
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  }
+app.get("/input", (req, res) => {
+  res.sendFile(__dirname + "/public/adduser.html");
+
 });
-const upload = multer({ storage: storage });
-
-// Handle file upload for /upload route
-app.post("/upload", upload.single('file'), (req, res) => {
-  // 'file' is the name attribute of the file input field in the HTML form
-  res.send("File uploaded successfully");
-});
-
-// Serve HTML files for specific routes
-app.get("/adduser", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "/adduser.html"));
-});
-
-// app.get("/delete", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "/delete.html"));
-// });
-
-// app.get("/edite", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "/edite.html"));
-// });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
